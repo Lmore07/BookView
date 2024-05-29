abfe8d30ef514335d479e650d355d64a05198e92
Lmore07 <85135347+Lmore07@users.noreply.github.com>
2024-05-29 19:43:22 +0000

    agregar boton de cancelar al context del listening

--- src/libs/contexts/speechToTextContext.tsx
+++ src/libs/contexts/speechToTextContext.tsx
@@ -1,9 +1,9 @@
 "use client";
+import ButtonOutlined from "@/ui/components/buttons/ButtonOutlined";
 import React, { createContext, useEffect, useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-
 import "./styles.css";
 
 export interface VoiceRecorderContextValue {
@@ -62,31 +62,43 @@ export const VoiceRecorderProvider = ({
     <VoiceRecorderContext.Provider value={{ setIsListening, finalTranscript }}>
       {isListening && (
         <div
-          className={`fixed flex z-[100] top-0 left-0 w-full h-full items-center justify-center`}
+          className={`fixed flex flex-col z-[100] top-0 left-0 w-full h-full items-center justify-center`}
         >
           <div className="absolute inset-0 bg-black bg-opacity-25 backdrop-blur-sm"></div>
-          <div className="relative z-10 flex bg-bgColorRight p-5 rounded-full">
-            <div
-              className={`h-8 w-4 bg-primary rounded-full mx-2 ${
-                isAnimating ? "animate-wave-1" : ""
-              }`}
-            ></div>
-            <div
-              className={`h-8 w-4 bg-primary rounded-full mx-2 ${
-                isAnimating ? "animate-wave-2" : ""
-              }`}
-            ></div>
-            <div
-              className={`h-8 w-4 bg-primary rounded-full mx-2 ${
-                isAnimating ? "animate-wave-3" : ""
-              }`}
-            ></div>
-            <div
-              className={`h-8 w-4 bg-primary rounded-full mx-2 ${
-                isAnimating ? "animate-wave-4" : ""
-              }`}
-            ></div>
+          <div className="relative z-10 flex flex-col bg-bgColorRight p-5 rounded-full">
+            <div className="flex flex-row">
+              <div
+                className={`h-8 w-4 bg-primary rounded-full mx-2 ${isAnimating ? "animate-wave-1" : ""
+                  }`}
+              ></div>
+              <div
+                className={`h-8 w-4 bg-primary rounded-full mx-2 ${isAnimating ? "animate-wave-2" : ""
+                  }`}
+              ></div>
+              <div
+                className={`h-8 w-4 bg-primary rounded-full mx-2 ${isAnimating ? "animate-wave-3" : ""
+                  }`}
+              ></div>
+              <div
+                className={`h-8 w-4 bg-primary rounded-full mx-2 ${isAnimating ? "animate-wave-4" : ""
+                  }`}
+              ></div>
+            </div>
+            <div className="flex items-center justify-center mt-3">
+              <ButtonOutlined
+              
+                onClick={() => {
+                  setIsListening(false);
+                }}
+                className={
+                  "border-red-600 text-red-600 hover:text-white hover:bg-red-600"
+                }
+              >
+                Detener
+              </ButtonOutlined>
+            </div>
           </div>
+
         </div>
       )}
       {children}

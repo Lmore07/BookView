617070880cecb20bf8203c8d5f2aecca68859381
Lmore07 <lmoreirat@uteq.edu.ec>
2024-06-06 06:23:47 -0500

    correcion de servicio para texto a voz

--- src/libs/services/generateSpeech.ts
+++ src/libs/services/generateSpeech.ts
@@ -1,11 +1,21 @@
-export async function generateSpeech(text: string){
+export async function generateSpeech(text: string) {
+  const body = {
+    text: text,
+    voice_settings: {
+      stability: 0.75,
+      similarity_boost: 0.5,
+      style: 0,
+      use_speaker_boost: true,
+    },
+    model_id: "eleven_multilingual_v2",
+  };
   const options = {
     method: "POST",
     headers: {
       "xi-api-key": "c5943e12f0d17a95d1864fc1d8ee0f17",
       "Content-Type": "application/json",
     },
-    body: `{"text":"${text}","voice_settings":{"stability":0.75,"similarity_boost":0.5,"style":0,"use_speaker_boost":true},"model_id":"eleven_multilingual_v2"}`,
+    body: JSON.stringify(body),
   };
   const response = await fetch(
     "https://api.elevenlabs.io/v1/text-to-speech/5Q0t7uMcjvnagumLfvZi",

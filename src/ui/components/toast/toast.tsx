8fd15c399139e4c4c9c92b1b3dc103a97e3f7e4c
Lmore07 <lmoreirat@uteq.edu.ec>
2024-06-08 03:47:26 -0500

    mejorar diseño del toast

--- src/ui/components/toast/toast.tsx
+++ src/ui/components/toast/toast.tsx
@@ -21,20 +21,29 @@ const Toast: React.FC<ToastProps> = ({
     return () => clearTimeout(timer);
   }, [duration, onClose]);
 
-  const close = () => {
-    setVisible(false);
+  const getTitle = () => {
+    switch (type) {
+      case ToastType.SUCCESS:
+        return "Éxito";
+      case ToastType.ERROR:
+        return "Error";
+      case ToastType.WARNING:
+        return "Advertencia";
+      case ToastType.INFO:
+        return "Información";
+    }
   };
 
   const getBackgroundColor = () => {
     switch (type) {
       case ToastType.SUCCESS:
-        return "bg-green-700";
+        return "emerald-500";
       case ToastType.ERROR:
-        return "bg-red-700";
+        return "red-500";
       case ToastType.WARNING:
-        return "bg-orange-600";
+        return "yellow-400";
       case ToastType.INFO:
-        return "bg-blue-900";
+        return "blue-500";
     }
   };
 
@@ -43,61 +52,41 @@ const Toast: React.FC<ToastProps> = ({
       case "success":
         return (
           <svg
+            className="w-6 h-6 text-white fill-current"
+            viewBox="0 0 40 40"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-5 h-5"
           >
-            <path
-              fillRule="evenodd"
-              d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-              clipRule="evenodd"
-            />
+            <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z" />
           </svg>
         );
       case "error":
         return (
           <svg
+            className="w-6 h-6 text-white fill-current"
+            viewBox="0 0 40 40"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-5 h-5"
           >
-            <path
-              fillRule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-              clipRule="evenodd"
-            />
+            <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
           </svg>
         );
       case "warning":
         return (
           <svg
+            className="w-6 h-6 text-white fill-current"
+            viewBox="0 0 40 40"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-5 h-5"
           >
-            <path
-              fillRule="evenodd"
-              d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-              clipRule="evenodd"
-            />
+            <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
           </svg>
         );
-      default:
+      case "info":
         return (
           <svg
+            className="w-6 h-6 text-white fill-current"
+            viewBox="0 0 40 40"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-5 h-5"
           >
-            <path
-              fillRule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-              clipRule="evenodd"
-            />
+            <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
           </svg>
         );
     }
@@ -105,29 +94,28 @@ const Toast: React.FC<ToastProps> = ({
 
   return (
     <div
-      className={`fixed flex items-center z-[100] top-4 right-4 rounded-md p-4 text-white ${getBackgroundColor()} ${
+      className={`fixed flex items-center z-[100] top-4 right-4 rounded-md p-4 text-white } ${
         visible ? "opacity-100" : "opacity-0"
       } transition-opacity duration-500`}
     >
-      <div className="pe-3">{getIcon()}</div>
-      <span>{message}</span>
-      <button
-        className="text-white ps-3 top-1 hover:text-gray-300 focus:outline-none"
-        onClick={close}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-5 h-5"
+      <div className="flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div
+          className={`flex items-center justify-center w-14 bg-${getBackgroundColor()}`}
         >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+          {getIcon()}
+        </div>
+
+        <div className="px-4 py-2">
+          <div className="">
+            <span className={`font-semibold text-${getBackgroundColor()}`}>
+              {getTitle()}
+            </span>
+            <div className="text-sm text-gray-600 dark:text-gray-200">
+              {message}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

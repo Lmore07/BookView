4446d7a6fe7c03a69ba3af2787d63ddadd58a431
Lmore07 <85135347+Lmore07@users.noreply.github.com>
2024-06-11 18:06:46 +0000

    correcion de agregar libro a favoritos

--- src/libs/dtos/folders/createFolderDTO.ts
+++ src/libs/dtos/folders/createFolderDTO.ts
@@ -15,17 +15,11 @@ export class CreateFolderDTO {
 }
 
 export class AddBookToFolder {
-  @IsNotEmpty({ message: "El nombre de la carpeta es requerido" })
-  @Length(1, 100, {
-    message: "El nombre de la carpeta debe tener entre 1 y 100 caracteres",
-  })
+  @IsNotEmpty({ message: "El libro es requerido" })
   @IsNumber({}, { message: "El id del libro debe ser un número" })
   idBook!: string;
 
-  @Length(1, 255, {
-    message: "La descripción de la carpeta debe tener entre 1 y 255 caracteres",
-  })
-  @IsNotEmpty({ message: "La descripción de la carpeta es requerida" })
-  @IsNumber({}, { message: "El id del libro debe ser un número" })
+  @IsNotEmpty({ message: "La carpeta es requerida" })
+  @IsNumber({}, { message: "La carpeta ser un número" })
   idFolder!: string;
 }

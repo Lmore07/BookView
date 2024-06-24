592a9471676126028e6c24949e5e1ca44822de47
Lmore07 <lmoreirat@uteq.edu.ec>
2024-06-24 09:08:19 -0500

    quitar seguridad al buscar definiciones

--- src/libs/services/generateText.ts
+++ src/libs/services/generateText.ts
@@ -1,8 +1,32 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import {
+  HarmBlockThreshold,
+  HarmCategory,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
 
 export async function generateText(text: string) {
   const genAI = new GoogleGenerativeAI(`${process.env.API_KEY_GEMINI}`);
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    safetySettings: [
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+    ],
+  });
   const prompt = `Quiero una definición corta, clara y sencilla, sobre la palabra: '${text}'`;
   const result = await model.generateContent(prompt);
   const textResult = result.response.text();
